5a83589ea64cdb47d639cfe733870ae2fb23ad71
Robert Passafaro <robert.passafaro@gmail.com>
2019-09-01 03:45:25 -0700

    added create and index for items that are associated with todos

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,26 @@
 class ItemsController < ApplicationController
+    before_action :set_item, only: [:show, :update, :destroy]
+    
+    def index
+        @items = Todo.find(params[:todo_id]).items
+        json_response(@items)
+    end
+    
+    def create
+        puts params
+        @item = Item.create!(item_params)
+        json_response(@item, :created)
+    end
+
+    # private
+
+    def item_params
+        #whitelist params
+        params.permit(:name, :todo_id)
+    end
+
+    def set_item
+        puts params
+        @item = Item.find(params[:id])
+    end
 end
